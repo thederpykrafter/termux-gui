c7295f84f45fdadf8f7ecb57661e22787789873d
thederpykrafter <thederpykrafter@gmail.com>
2025-04-04 10:53:03 -0400

    update widget

--- widget.py
+++ widget.py
@@ -1,11 +1,11 @@
-#widget = "ab5d20e2-9b2c-4e7e-b755-d77bf5a2d69d"
+#!/usr/bin/env python3
 import sys
 import termuxgui as tg
 import time
 import os
 
 with tg.Connection() as c:
-    wid = sys.argv[1]
+    wid = "ab5d20e2-9b2c-4e7e-b755-d77bf5a2d69d"
     # Create a remote layout
     rv = tg.RemoteViews(c)
     # Add a TextView
